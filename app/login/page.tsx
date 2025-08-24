13263110dce68f2a463c84af3400d73f93bc56d2
Zynalex9 <alzn952@gmail.com>
2025-08-24 17:25:06 +0500

    Login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
-import React from "react";
+import { SignInForm } from "./LoginForm";
 
 export default function page() {
-  return <div>ARE YOU REALLY HEERE???? AT LOGIN???????</div>;
+  return <SignInForm/>
 }
